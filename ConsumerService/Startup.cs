6d78deb1c9e8761700235b205c7e120426d0d2a6
Tona Diaz <tdiaz@healthwise.org>
2020-01-05 21:07:34 -0700

    Changed version of the apps to .NET Core 2.2. Until Microsoft fixes the Azure Dev Spaces Bug. https://github.com/Azure/dev-spaces/issues/263

--- ConsumerService/Startup.cs
+++ ConsumerService/Startup.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
+using ConsumerService.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace ConfigurationService
+namespace ConsumerService
 {
     public class Startup
     {
@@ -26,6 +21,10 @@ namespace ConfigurationService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHttpClient();
+
+            const string configurationServiceUrl = "http://configuration";
+            services.AddTransient<ConsumersController>(injector => new ConsumersController((HttpClient)injector.GetService(typeof(HttpClient)), configurationServiceUrl));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
